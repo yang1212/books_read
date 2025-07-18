4d75851f9ba61c099ad15d06ae1627651d18b288
杨伏 <1204914685@qq.com>
2025-07-18 13:56:31 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+export default {
+  title: '📚 我的书籍库',
+  description: '分类书籍整理导航',
+  themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/yang1212/books_read' }
+    ]
+  }
+}
